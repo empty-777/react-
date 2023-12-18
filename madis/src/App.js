f637bd3fa8dc0d33f1af1688fcd6df1fcb00340c
empty-777 <116153110+empty-777@users.noreply.github.com>
2023-12-18 14:28:08 +0530

    Add files via upload

---
+++ madis/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+import "./components/scss/main.css";
+function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default App
---
+++ madis/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+    <div className="container">
+    <div className='header-top'>
+        <a href="">Value at your desk. contact us →</a>
+        <ul className="ul-help">
+            <li>help desk</li>
+            <li> Emergency</li>
+            <li> Contact</li>
+        </ul>
+        </div>
+    </div>
+        </header>
+    )
+}
+
+export default Header
